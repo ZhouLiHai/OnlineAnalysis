f3981252e3c5bc0535522701ed009800a38c8cd2
ZhouLiHai <practicezhou@gmail.com>
2018-07-12 16:50:19 +0800

    补充了因为jdk问题而无法测试的range实例

--- src/main/java/GuavaExample/RangeExample.java
+++ src/main/java/GuavaExample/RangeExample.java
@@ -16,6 +16,40 @@ public class RangeExample {
 		Range<Integer> range1 = Range.closed(0,9);
 		System.out.println("[0,9]:");
 		printRange(range1);
+
+		System.out.println();
+
+		// 闭开区间
+		Range<Integer> range2 = Range.closedOpen(0,9);
+		System.out.println("[0, 9):");
+		printRange(range2);
+
+		System.out.println();
+
+		//可以定义一个9到无限大的数组，可以知道range应该没有被预先分配
+		Range<Integer> range3 = Range.greaterThan(9);
+		//有最小值
+		System.out.println(range3.lowerEndpoint());
+		//但是没有最大值
+		System.out.println(range3.hasUpperBound());
+
+		System.out.println("=========");
+
+		// 只要有交集计算是链接的
+		System.out.println(range1.isConnected(Range.closed(8, 20)));
+		System.out.println(range1.isConnected(Range.closed(9, 20)));
+		// 哪怕是开区间
+		System.out.println(range1.isConnected(Range.openClosed(9, 20)));
+		System.out.println(range1.isConnected(Range.closed(10, 20)));
+
+		System.out.println("=========");
+
+		Range<Integer> range8 = Range.closed(5, 15);
+		// 交集
+		System.out.println(range1.intersection(range8));
+		// 合集
+		System.out.println(range1.span(range8));
+
 	}
 
 	private void printRange(Range<Integer> range) {
